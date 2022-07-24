cdedb85ca7c482b5364dcabbf7eb2d76e853324c
JaQwaee <jaqwaeellison@gmail.com>
2022-07-24 14:25:11 -0500

    local storage is working

--- asset/scripts.js
+++ asset/scripts.js
@@ -3,7 +3,8 @@ currentDate = moment().format('');
 currentDate = document.querySelector('#currentDay');
 currentDay.textContent = moment().format('dddd, MMM, Do, YYYY, h:mm:ss');
 
-//Keeping time up to date without a refresh
+
+//Keeping time up current without a refresh
 function updatedTime () {
     var timerInterval = setInterval(function() {
         currentDate = moment().format('dddd, MMM, Do, YYYY, h:mm:ss');
@@ -12,74 +13,60 @@ function updatedTime () {
 }
 updatedTime();
 
-// WHEN I view the time blocks for that day
-// THEN each time block is color-coded to indicate whether it is in the past, present, or future
+
+//tells engine to load html and css first
 $(document).ready(function(){
     $('.saveBtn').on('click',function () {
-        console.log(this);
-        var time = $(this).siblings(".description").val();
-        var time = $(this).parent().attr('id');
-
-    localStorage.setItem(time, text);
-    })
-})
-// keep track of the current hour
-function hourTracker() {
-
-    var currentHour = moment().hour();
-    
-    $(".time-block").each( function() {
-        var elementHour = parseInt($(this).attr("id").split('hour')[1]);
+        //save text input
+        var text = $(this).siblings(".description").val();
+        //save time id
+        var time = $(this).parent().attr('id'); 
         
-        // hour is styled to the past class
-        if ( elementHour < currentHour ) {
-            $(this).addClass("past");
-            $(this).removeClass("present");
-            $(this).removeClass("future");
-        }
-        // hour is styled to the present class
-        else if ( elementHour === currentHour ) {
-            $(this).removeClass("past");
-            $(this).addClass("present");
-            $(this).removeClass("future");
-        }
-        // hour is styled to the future class
-        else {
-            $(this).removeClass("past");
-            $(this).removeClass("present");
-            $(this).addClass("future");
-        }
+        //setting items in local storage
+        localStorage.setItem(time, text);
     })
-};
-hourTracker()
 
-$("#hour9 .description").val(localStorage.getItem("hour9"));
-// WHEN I scroll down
-// THEN I am presented with time blocks for standard business hours
-//create rows for each hour from 9am-5pm
-//start with 3 rows (until fully functioning)
-//make columns within rows
-//left side displays time
-//center is where the task go
-//right is where the save button is
+    // keep track of the current hour
+    function hourTracker() {
 
+        var currentHour = moment().hour();
+        
+        // loop over time block
+        $(".time-block").each( function() {
+            var elementHour = parseInt($(this).attr("id").split('hour')[1]);
+            
+            // hour has past and is styled to the past class
+            if ( elementHour < currentHour ) {
+                $(this).addClass("past");
+                $(this).removeClass("present");
+                $(this).removeClass("future");
+            }
+            // hour is current and is styled to the present class
+            else if ( elementHour === currentHour ) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("future");
+            }
+            // hour has not yet come and is styled to the future class
+            else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+        })
+    }
 
+    //get items from local storage
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+    $("#hour0 .description").val(localStorage.getItem("hour0"));
 
-
-// WHEN I click into a time block
-// THEN I can enter an event
-//add event listener where task go
-    //display text on that corresponding size
-
-
-// WHEN I click the save button for that time block
-// THEN the text for that event is saved in local storage
-    //create a button for save
-        //add event listener
-    //link to local storage
-        //set value inputted in task section 
-
-
-// WHEN I refresh the page
-// THEN the saved events persist
-    //write out some step for local storage as well as displaying (look at activity 26 in web API for assistance)
+    hourTracker();
+})
